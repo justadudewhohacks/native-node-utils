1c1d6ac6d38ad7a8c64ad9fdce26ac43943db04f
vincent <muehler.v@gmail.com>
2018-04-10 14:14:50 +0200

    wrap stuff in classes to prevent redefinition of symbols

--- src/NativeNodeUtils.h
+++ src/NativeNodeUtils.h
@@ -11,15 +11,18 @@
 
 namespace FF {
 
-	static std::string formatError(std::string methodName, std::string err) {
-		return std::string(methodName) + std::string(" - ") + err;
-	}
+	class Utils {
+	public:
+		static std::string formatError(std::string methodName, std::string err) {
+			return std::string(methodName) + std::string(" - ") + err;
+		}
+	};
 
 	class TryCatch : public Nan::TryCatch {
 	public:
 		v8::Local<v8::String> formatCatchedError(std::string methodName) {
 			return Nan::New(
-				formatError(
+				Utils::formatError(
 					std::string(methodName),
 					*Nan::Utf8String(Exception()->ToString())
 				)
@@ -33,39 +36,55 @@ namespace FF {
 		}
 	};
 
-	void SyncBinding(std::shared_ptr<IWorker> worker, std::string methodName, Nan::NAN_METHOD_ARGS_TYPE info) {
-		FF::TryCatch tryCatch;
-
-		if (worker->applyUnwrappers(info)) {
-			v8::Local<v8::Value> err = tryCatch.formatCatchedError(methodName);
-			tryCatch.throwNew(err);
-			return;
+	struct SyncBinding {
+	public:
+		SyncBinding(std::shared_ptr<IWorker> worker, std::string methodName, Nan::NAN_METHOD_ARGS_TYPE info) {
+			run(worker, methodName, info);
 		}
 
-		std::string err = worker->execute();
-		if (!err.empty()) {
-			tryCatch.throwNew(Nan::New(formatError(methodName, err)).ToLocalChecked());
-			return;
+	private:
+		void run(std::shared_ptr<IWorker> worker, std::string methodName, Nan::NAN_METHOD_ARGS_TYPE info) {
+			FF::TryCatch tryCatch;
+
+			if (worker->applyUnwrappers(info)) {
+				v8::Local<v8::Value> err = tryCatch.formatCatchedError(methodName);
+				tryCatch.throwNew(err);
+				return;
+			}
+
+			std::string err = worker->execute();
+			if (!err.empty()) {
+				tryCatch.throwNew(Nan::New(Utils::formatError(methodName, err)).ToLocalChecked());
+				return;
+			}
+
+			info.GetReturnValue().Set(worker->getReturnValue());
+		}
+	};
+
+	struct AsyncBinding {
+	public:
+		AsyncBinding(std::shared_ptr<IWorker> worker, std::string methodName, Nan::NAN_METHOD_ARGS_TYPE info) {
+			run(worker, methodName, info);
 		}
 
-		info.GetReturnValue().Set(worker->getReturnValue());
-	}
-
-	void AsyncBinding(std::shared_ptr<IWorker> worker, std::string methodName, Nan::NAN_METHOD_ARGS_TYPE info) {
-		FF::TryCatch tryCatch;
-		if (!hasArg(info.Length() - 1, info) || !info[info.Length() - 1]->IsFunction()) {
-			tryCatch.throwNew(Nan::New(formatError(methodName, "callback function required")).ToLocalChecked());
-			return;
+	private:
+		void run(std::shared_ptr<IWorker> worker, std::string methodName, Nan::NAN_METHOD_ARGS_TYPE info) {
+			FF::TryCatch tryCatch;
+			if (!hasArg(info.Length() - 1, info) || !info[info.Length() - 1]->IsFunction()) {
+				tryCatch.throwNew(Nan::New(Utils::formatError(methodName, "callback function required")).ToLocalChecked());
+				return;
+			}
+			Nan::Callback *callback = new Nan::Callback(info[info.Length() - 1].As<v8::Function>());
+			if (worker->applyUnwrappers(info)) {
+				v8::Local<v8::Value> argv[] = { Nan::Error(tryCatch.formatCatchedError(methodName)), Nan::Null() };
+				tryCatch.Reset();
+				callback->Call(2, argv);
+				return;
+			}
+			Nan::AsyncQueueWorker(new FF::AsyncWorker(callback, worker));
 		}
-		Nan::Callback *callback = new Nan::Callback(info[info.Length() - 1].As<v8::Function>());
-		if (worker->applyUnwrappers(info)) {
-			v8::Local<v8::Value> argv[] = { Nan::Error(tryCatch.formatCatchedError(methodName)), Nan::Null() };
-			tryCatch.Reset();
-			callback->Call(2, argv);
-			return;
-		}
-		Nan::AsyncQueueWorker(new FF::AsyncWorker(callback, worker));
-	}
+	};
 
 }
 
